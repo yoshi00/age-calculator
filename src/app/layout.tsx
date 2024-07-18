88ade02a962b4bc9204b8ee42b6252bec7323bce
yoshi00 <jvsamora00@gmail.com>
2024-07-18 20:47:48 -0300

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <title>Age Calculator</title>
       </head>
       <body>{children}</body>
