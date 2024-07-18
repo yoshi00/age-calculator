93c778a4111f1ee445d9b9c5311fdaf4ebbbaa1a
yoshi00 <jvsamora00@gmail.com>
2024-07-18 20:40:36 -0300

    change layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Age Calculator</title>
+      </head>
       <body>{children}</body>
     </html>
   )
